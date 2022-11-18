8d866c91c9f215d840138f0970ff323ed0b7c8dc
rayanne-barros <rayanneb126@gmail.comom>
2022-11-17 21:20:33 -0300

    Método para depositar valor para uma conta

---
+++ aula_4/src/Conta.java
@@ -0,0 +1,35 @@
+public class Conta {
+    private Double saldo;
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void depositar(Double valor){
+        saldo += valor;
+    }
+
+    public void verificaSaldo(){
+        System.out.println("Valor do Saldo: "+getSaldo());
+    }
+    public static void main(String[] args) {
+
+        //INSTANCIA A CLASSE
+        Conta conta = new Conta();
+
+        //DEFINE UM VALOR DE SALDO
+        conta.setSaldo(633.00);
+
+        //DEFINE VALOR PARA DEPOSITAR
+        conta.depositar(555.0);
+
+        //RESGATA VALOR
+        conta.verificaSaldo();
+
+    }
+}
+
